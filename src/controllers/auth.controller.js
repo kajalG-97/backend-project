026eb5cc92ac7f368c7c770af83e7ca25a357e88
Kajal <kajal.ganorkar@connectandheal.com>
2024-05-05 21:04:02 +0530

    error kept outside of try catch block

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -21,15 +21,16 @@ export const generateToken = (payload, expiryTime = "10d", extra = {}) => {
 };
 
 export const register = async (req, res) => {
+  // extracting error from req
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    let newErrors = errors.array().map((err) => {
+      return { key: err.param, message: err.msg };
+    });
+    return res.status(400).send({ errors: newErrors });
+  }
+
   try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      let newErrors;
-      newErrors = errors.array().map((err) => {
-        return { key: err.param, message: err.msg };
-      });
-      return res.status(400).send({ errors: newErrors });
-    }
     let user = await User.findOne({ email: req.body.email }).lean().exec();
 
     if (user)
@@ -46,18 +47,20 @@ export const register = async (req, res) => {
 };
 
 export const login = async (req, res) => {
+  // extracting error from req
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const newErrors = errors.array().map((err) => {
+      return { key: err.param, message: err.msg };
+    });
+    return res.status(400).send({ errors: newErrors });
+  }
+
   try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      let newErrors;
-      newErrors = errors.array().map((err) => {
-        return { key: err.param, message: err.msg };
-      });
-      return res.status(400).send({ errors: newErrors });
-    }
     const user = await User.findOne({ email: req.body.email });
 
     if (!user)
+      // need to check
       return res
         .status(400)
         .send({ message: "Please try another email or password" });
